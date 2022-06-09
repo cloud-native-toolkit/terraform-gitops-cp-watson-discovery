3075a472883b89574cc641e8ef034c550163004d
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-09 11:55:36 +0530

    Watson Discvoery Deployment automation
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage2-wd.tf
+++ test/stages/stage2-wd.tf
@@ -1,4 +1,5 @@
-module "gitops_module" {
+module "cp-watson-discovery" {
+ 
   source = "./module"
 
   gitops_config = module.gitops.gitops_config
@@ -6,4 +7,6 @@ module "gitops_module" {
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
+  operator_namespace= "cpd-operators"
+  cpd_namespace = "gitops-cp4d-instance"
 }
